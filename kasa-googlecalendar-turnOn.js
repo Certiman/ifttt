aa990f7aaa8087ec0426e99f24b360bc9d2fb52e
@certiman <maarten.duhoux@gmail.com>
2021-11-17 22:19:52 +0100

    Update kasa-googlecalendar-turnOn.js

--- kasa-googlecalendar-turnOn.js
+++ kasa-googlecalendar-turnOn.js
@@ -1,6 +1,9 @@
 /*
- * ANother script creates events in the IFTTT calendar with the title containing the room to light up.
+ * Some other script creates events in the IFTTT calendar with the title containing the room to light up.
  * This script can only skip one room because of the way the IF is written.
+ * Per room to light up, an event should therefore be created, at the exact moment WHEN the Kasa lights are turned on there.
+ */
+
 let inRoomM = GoogleCalendar.eventFromSearchStarts.Title.search("room M") > 0
 let inRoomA = GoogleCalendar.eventFromSearchStarts.Title.search("room A") > 0
 let inRoomB = GoogleCalendar.eventFromSearchStarts.Title.search("room B") > 0
@@ -11,12 +14,12 @@ if ( inRoomM ){
   Kasa.turnOn3.skip() // do not light room B
 }else if ( inRoomA ){
   Kasa.turnOn1.skip()
-  // Kasa.turnOn2.skip()
+  // Kasa.turnOn2.skip() // light room A
   Kasa.turnOn3.skip()
 }else if ( inRoomB ){
   Kasa.turnOn1.skip()
   Kasa.turnOn2.skip()
-  // Kasa.turnOn3.skip()
+  // Kasa.turnOn3.skip() // light room B
 }
 
 // Ugliest ever
