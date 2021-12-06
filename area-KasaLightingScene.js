e82677c49dca7c63934e7e36489f771e07d64efa
@certiman <maarten.duhoux@gmail.com>
2021-12-06 19:16:56 +0100

    Update area-KasaLightingScene.js

--- area-KasaLightingScene.js
+++ area-KasaLightingScene.js
@@ -1,9 +1,14 @@
-// Variables will need to be HH:MM strings for comparison
-let hourSunrise = Weather.currentWeather[0].SunriseAt.substr(11,5)
-let hourSunset = Weather.currentWeather[0].SunsetAt.substr(11,5)
-let currentHour = Meta.currentUserTime.hour()+":"+Meta.currentUserTime.minute()
+// FAILS - Variables will need to be HH:MM strings for comparison
+// Variables will need to be Moment objects for comparison
+let timeSunrise = moment(new Date(Weather.currentWeather[0].SunriseAt))
+let timeSunset = moment(new Date(Weather.currentWeather[0].SunsetAt))
+let currentTime = Meta.currentUserTime
 
-if (currentHour < hourSunset || currentHour > hourSunrise){
+let sunIsStillShining = ( 
+  currentTime.isBefore(timeSunset) && currentTime.isAfter(timeSunrise)
+)
+
+if (sunIsStillShining){
   Kasa.activateScene.skip() // Skip the scene when sun has risen and not set yet
   IfNotifications.sendRichNotification.setTitle("No lighting scene was activated")
   IfNotifications.sendRichNotification.setMessage("With sunrise at "+hourSunrise+" and sunset at "+hourSunset+", your arrival-triggered lighting scene was not activated at "+currentHour+"!")
